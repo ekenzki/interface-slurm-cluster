f8e54d2f1ea41d04e19b410982128fd07ab942d5
Dmitrii Shcherbakov <dmitrii.shcherbakov@canonical.com>
2018-07-24 15:24:15 +0300

    fix active-backup controller handling logic
    Currently a backup controller config is returned as the active one due
    to a logical error.
    
    This change adds more logging and fixes the issue.

--- requires.py
+++ requires.py
@@ -1,9 +1,7 @@
 import charms.reactive.flags as flags
 import charms.reactive as reactive
-import charms.reactive.helpers as helpers
 
 from charmhelpers.core.hookenv import (
-    DEBUG,
     log,
 )
 
@@ -27,6 +25,7 @@ class SlurmRequires(reactive.Endpoint):
         # can only handle a single controller relation both active and
         # standby receive the same node info for this relation
         rel = self._controller_relation()
+        log('Sending node info')
         rel.to_publish.update({
             'hostname': hostname,
             'partition': partition,
@@ -39,12 +38,14 @@ class SlurmRequires(reactive.Endpoint):
         if config:
             log('Determining readiness by nodes from config: {}'.format(
                 config.get('nodes')))
-            for node in config.get('nodes'):
+            nodes = config.get('nodes')
+            for node in nodes:
                 if node['ingress_address'] == self.ingress_address:
                     log('The controller is ready')
                     return True
         else:
-
+            log('Controller is not ready as config is empty: {}'.format(
+                repr(config)))
             return False
 
     @reactive.when('endpoint.{endpoint_name}.changed')
@@ -56,9 +57,15 @@ class SlurmRequires(reactive.Endpoint):
         if self._controller_config_ready(self._active_data):
             flags.set_flag(self.expand_name(
                 'endpoint.{endpoint_name}.active.available'))
+            log('Set {} flag'.format(self.expand_name(
+                'endpoint.{endpoint_name}.active.available')))
             flags.set_flag(self.expand_name(
                 'endpoint.{endpoint_name}.active.changed'))
+            log('Set {} flag'.format(self.expand_name(
+                'endpoint.{endpoint_name}.active.changed')))
         else:
+            log('Controller config not ready, clearing active.available'
+                ' and active.changed flags')
             self.controller_broken()
             # TODO: JSON is not serializable => need to either remove
             # this and execute more or solve the problem
@@ -68,14 +75,22 @@ class SlurmRequires(reactive.Endpoint):
 
         # processed the relation changed event - can clear this flag now
         flags.clear_flag(self.expand_name('changed'))
+        log('Cleared {} flag'.format(self.expand_name('changed')))
 
     @reactive.when('endpoint.{endpoint_name}.departed')
     def controller_broken(self):
         flags.clear_flag(self.expand_name(
             'endpoint.{endpoint_name}.active.available'))
+        log('Cleared {} flag'.format(self.expand_name(
+            'endpoint.{endpoint_name}.active.available')))
+
         flags.clear_flag(self.expand_name(
             'endpoint.{endpoint_name}.active.changed'))
+        log('Cleared {} flag'.format(self.expand_name(
+            'endpoint.{endpoint_name}.active.changed')))
+
         flags.clear_flag(self.expand_name('departed'))
+        log('Cleared {} flag'.format(self.expand_name('departed')))
 
     @property
     def active_data(self):
@@ -84,7 +99,9 @@ class SlurmRequires(reactive.Endpoint):
     def _controller_config(self):
         rel = self._controller_relation()
 
+        log('Joined controller units: {}'.format(rel.joined_units))
         partitions = None
+        recv_active = None
         for u in rel.joined_units:
             recv = u.received
             log('Received from {}: {}'.format(u.unit_name, recv))
@@ -92,13 +109,19 @@ class SlurmRequires(reactive.Endpoint):
             # post any config and there will only be one config posted
             # by the active controller
             cpartitions = recv.get('partitions')
+            log('partitions: {}'.format(repr(partitions)))
+            log('cpartitions: {}'.format(repr(cpartitions)))
             if partitions and cpartitions:
+                log('Two controllers presenting active data: split-brain')
                 # catch a split-brain condition when two controllers
                 # advertise possibly conflicting config data which means
                 # that active controller change
                 # TODO: error/status handling for this
                 flags.set_flag(self.expand_name(
                     'endpoint.{endpoint_name}.split-brain'))
-            else:
+            elif not partitions:
+                log('Controller partitions: {}'.format(cpartitions))
                 partitions = cpartitions
-        return recv if partitions else {}
+                recv_active = recv
+        log('Active controller partitions: {}'.format(partitions))
+        return recv_active if partitions else {}
